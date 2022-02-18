f1be20207e548a32e5800df7944ca87d5317642f
Client <client@Clients-MacBook-Air.local>
2022-02-18 16:22:24 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 // import Hand from "./components/Hand";
 import About from "./components/About";
+import { FaArrowDown } from 'react-icons/fa';
 
 function App() {
   return (
@@ -14,6 +15,12 @@ function App() {
       </div>
       <h1>Hi i'm Jesse Beke</h1>
       <p>Frontend developer / UI/UX Designer</p>
+      <div class="container">
+        <span class="circle">
+          <FaArrowDown className="fa"/>
+        </span>
+        <span class="pulse"></span>
+      </div>
       <About />
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,12 @@ import "../App.css";
 
 
 function About() {
-    return (
-      <div className="about">
-      </div>
-    );
-  }
-  
-  export default About;
+  return (
+    <div className="about">
+      <h2>Who is Jesse?</h2>
+      <img src='question.png' alt='question-mark' />
+    </div>
+  );
+}
+
+export default About;
